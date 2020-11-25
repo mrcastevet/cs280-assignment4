907d4c54604b8ea8c49cb506ebab5b09618dbb0d
Shane Arcaro <smarcaro@gmail.com>
2020-11-24 20:04:14 -0500

    Adding tester file

---
+++ Tester.cpp
@@ -0,0 +1,10 @@
+/*
+ * File: Tester.cpp
+ * Project: Assignment4
+ * File Created: Tuesday, 24th November 2020 7:59:20 pm
+ * Author: Shane Arcaro (sma237@njit.edu)
+ */
+
+int main(int argc, char** argv) {
+    return 0;
+}
